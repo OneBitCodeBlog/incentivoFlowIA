8982be40dbb54b39bd658184b682ee27ba569f44
Léo Scorza <contato@onebitcode.com>
2024-07-19 16:53:44 +0200

    4 - dados fake

--- config/admin.js
+++ config/admin.js
@@ -8,6 +8,7 @@ import LeadCampaign from '../models/leadCampaign.js';
 import campaignResourceOptions from './campaignResourceOptions.js';
 import leadResourceOptions from './leadResourceOptions.js';
 import leadCampaignResourceOptions from './leadCampaignResourceOptions.js';
+import branding from './branding.js';
 import { Router } from 'express';
 
 // Registrar o adaptador do Sequelize
@@ -30,43 +31,7 @@ const adminJs = new AdminJS({
         }
     ],
     rootPath: process.env.ADMINJS_ROOT_PATH,
-    branding: {
-        companyName: 'IncentivoFlow',
-        logo: false,  // Pode adicionar uma URL para um logo se desejar
-        softwareBrothers: false,
-        theme: {
-            colors: {
-                primary100: '#000000',
-                primary80: '#333333',
-                primary60: '#666666',
-                primary40: '#999999',
-                primary20: '#CCCCCC',
-                accent: '#000000',
-                love: '#FF0000',
-                grey100: '#151515',
-                grey80: '#333333',
-                grey60: '#4D4D4D',
-                grey40: '#666666',
-                grey20: '#E2E2E2',
-                white: '#FFFFFF',
-                errorDark: '#FF0000',
-                error: '#FF3333',
-                errorLight: '#FF6666',
-                successDark: '#00FF00',
-                success: '#33FF33',
-                successLight: '#66FF66',
-                infoDark: '#0000FF',
-                info: '#3333FF',
-                infoLight: '#6666FF',
-                bg: '#1F1F1F', // Login page background color
-                hoverBg: '#2C2C2C', // Sidebar item hover background color
-                border: '#333333', // Sidebar border color
-                inputBorder: '#4D4D4D', // Input border color
-                separator: '#333333', // Sidebar separator color
-                highlight: '#666666', // Sidebar highlight color
-            },
-        },
-    },
+    branding: branding,
 });
 
 const router = Router();
@@ -76,7 +41,7 @@ const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
         if (email === process.env.ADMINJS_USERNAME && password === process.env.ADMINJS_PASSWORD) {
             return Promise.resolve({ email });
         }
-        return Promise.reject();
+        return null; // Retorna null para falha na autenticação
     },
     cookiePassword: 'some-secret-password-used-to-secure-cookie',
 });
---
+++ config/branding.js
@@ -0,0 +1,39 @@
+const branding = {
+  companyName: 'IncentivoFlow',
+  logo: false,  // Pode adicionar uma URL para um logo se desejar
+  softwareBrothers: false,
+  theme: {
+      colors: {
+          primary100: '#000000',
+          primary80: '#333333',
+          primary60: '#666666',
+          primary40: '#999999',
+          primary20: '#CCCCCC',
+          accent: '#000000',
+          love: '#FF0000',
+          grey100: '#151515',
+          grey80: '#333333',
+          grey60: '#4D4D4D',
+          grey40: '#666666',
+          grey20: '#E2E2E2',
+          white: '#FFFFFF',
+          errorDark: '#FF0000',
+          error: '#FF3333',
+          errorLight: '#FF6666',
+          successDark: '#00FF00',
+          success: '#33FF33',
+          successLight: '#66FF66',
+          infoDark: '#0000FF',
+          info: '#3333FF',
+          infoLight: '#6666FF',
+          bg: '#1F1F1F', // Login page background color
+          hoverBg: '#2C2C2C', // Sidebar item hover background color
+          border: '#333333', // Sidebar border color
+          inputBorder: '#4D4D4D', // Input border color
+          separator: '#333333', // Sidebar separator color
+          highlight: '#666666', // Sidebar highlight color
+      },
+  },
+};
+
+export default branding;
